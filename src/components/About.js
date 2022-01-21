2597234b501773ff7cb298d7a80fea02f1ed1129
Tyler Norman <tnorman600@gmail.com>
2022-01-21 14:02:18 -0700

    about me and contact edits

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,13 @@ import React from "react";
 function About() {
   return (
     <div>
-      <p>Full Stack Developer</p>
-      <hr />
-      <img className="" src="" alt="Tyler Norman"/>
+      <img src="https://pbs.twimg.com/profile_images/944642490010923008/QufVk-KY_400x400.jpg" alt="Tyler Norman"/>
       <p>
-        about me text here
+        Hello, and welcome! I am a Full Stack Developer and long time leader. I have experience with the MERN (MongoDB, Express.js, React, Node.js) stack and multiple years of experience in management of people, leaders, and projects.
+        I've gratuated from The University of Utah's Full Stack Dev. coding bootcamp and invite you to browse my works, download my resume, and contact me with any job offers.
       </p>
       <p>
-        about me text here
+        Thank you for visiting!
       </p>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,63 +1,34 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../utils/helpers';
+import React from 'react';
+import { useForm } from "react-hook-form";
 
 function Contact() {
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
-    }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`A ${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
+  const { register, formState: { errors }, handleSubmit, reset } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
     <div>
       <p>Contact</p>
-      <hr />
-      <form id="contact-form" onSubmit={handleSubmit}>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label className="label" htmlFor="name">Name</label>
-          <input className="input" type="text" name="name" defaultValue={name} onBlur={handleChange} placeholder='Jane Doe' />
+          <label>Name: </label>
+          <input type="text" name="name" placeholder='Jane Doe' {...register("name", {required: true, maxLength: 20})} />
+          {errors.name?.type === 'required' && "Name required."}
         </div>
+
         <div>
-          <label className="label" htmlFor="email">Email Address</label>
-          <input className="input" type="email" name="email" defaultValue={email} onBlur={handleChange} placeholder='jane@mail.com' />
+          <label>Email Address: </label>
+          <input type="email" name="email" placeholder='jane@mail.com' {...register("email", {required: true, maxLength: 50, pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/})} />
+          {errors.email?.type === 'required' && "Email required."}
         </div>
+
         <div>
-          <label className="label" htmlFor="message">Message</label>
-          <textarea className="textarea" name="message" rows="5" defaultValue={message} onBlur={handleChange} placeholder="Wow! You're so cool, Tyler. We'd love to offer you a job..." />
+          <label>Message: </label>
+          <textarea type="text" name="message" rows="5" placeholder="Wow! You're so cool, Tyler. We'd love to offer you a job..." {...register("message", {required: true, maxLength: 300})} />
+          {errors.message?.type === 'required' && "Message required."}
         </div>
-        {errorMessage && (
-          <div>
-            <p>{errorMessage}</p>
-          </div>
-        )}
-        <button type="submit">Submit</button>
+
+        <input type="Submit" />
       </form>
     </div>
   );
