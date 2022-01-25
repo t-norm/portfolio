d3322321aec9503eda6374c5ef0db203e9d7fd98
Tyler Norman <tnorman600@gmail.com>
2022-01-24 22:33:44 -0700

    final styling precheck for challenge acceptance criteria

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import resume from '../assets/Resume.pdf';
 function Resume() {
   return (
     <div className="bg-light">
-      <iframe width="100%" height="760px" src={resume} ></iframe>
+      <iframe width="100%" height="800px" src={resume} ></iframe>
     </div>
   );
 }
